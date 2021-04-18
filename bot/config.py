f578685c30e2cf60b60055f35d8865ac1ee9e22f
dhilipvtd <82697464+dhilipvtd@users.noreply.github.com>
2021-04-18 11:50:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,8 @@ class config:
     SUDO_USERS = "" # Sepearted by space.
     SUPPORT_CHAT_LINK = " 1111923902"
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = "cartoontamilzan@gmail.com"
-    G_DRIVE_CLIENT_SECRET = "dhilipkumar"
+    G_DRIVE_CLIENT_ID = ""
+    G_DRIVE_CLIENT_SECRET = ""
 
 
 class BotCommands:
