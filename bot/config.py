d6bcb0900e7ce6381ef99eeb361a36c404492bfa
dhilipvtd <82697464+dhilipvtd@users.noreply.github.com>
2021-04-18 10:15:11 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,8 @@ class config:
     SUDO_USERS = "" # Sepearted by space.
     SUPPORT_CHAT_LINK = " 1111923902"
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = "54476014721-foc7dalbtie30m6uf1kds0qe1p44ll1m.apps.googleusercontent.com"
-    G_DRIVE_CLIENT_SECRET = "7SGkf3PEDmybliJ7bnP9alaz"
+    G_DRIVE_CLIENT_ID = "54476014721-vupn6l2ijbgpoedu2t5t98ahl22jcpcv.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "svCMqvxkT81wHTGyTA84IEuE"
 
 
 class BotCommands:
